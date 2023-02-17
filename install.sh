b1269ff2afb2dfd86791b50eb858163e18f2e5c1
Logan M Hood <hoodlma@gmail.com>
2023-02-16 22:38:04 -0500

    install uses compare flag to avoid cluttering with unnecessary backups

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ dotfile_install() {
   local src_file="$1"
   local dest_file="$2"
 
-  echo "installing $src_file..."
-  install --mode=444 --backup=numbered --verbose "$src_file" "$dest_file"
+  echo "Check if $src_file has changed..."
+  install --compare --mode=444 --backup=numbered --verbose "$src_file" "$dest_file"
 }
 
 dotfile_install "vimrc"  "$HOME/.vimrc"
